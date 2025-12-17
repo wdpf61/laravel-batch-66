b30570bf2717c13075fa678f945a71d34b2e0f74
Wdpf-66 <ccsl61588@gmail.com>
2025-12-17 09:50:11 +0600

    migration , seed , Factory,  Controller , model

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // DB::table("customers")->insert([
+        //     ["name" => "Hasan", "email" => "hasan@gmail.com", "phone" => "123456", "address" => "Dhaka", "photo" => "hasan.jpg"],
+        //     ["name" => "Rafi", "email" => "rafi@gmail.com", "phone" => "111222", "address" => "Chittagong", "photo" => "rafi.jpg"],
+        //     ["name" => "Sakib", "email" => "sakib@gmail.com", "phone" => "222333", "address" => "Sylhet", "photo" => "sakib.jpg"],
+        //     ["name" => "Imran", "email" => "imran@gmail.com", "phone" => "333444", "address" => "Khulna", "photo" => "imran.jpg"],
+        //     ["name" => "Mamun", "email" => "mamun@gmail.com", "phone" => "444555", "address" => "Rajshahi", "photo" => "mamun.jpg"],
+        //     ["name" => "Jahid", "email" => "jahid@gmail.com", "phone" => "555666", "address" => "Barishal", "photo" => "jahid.jpg"],
+        //     ["name" => "Farhan", "email" => "farhan@gmail.com", "phone" => "666777", "address" => "Rangpur", "photo" => "farhan.jpg"],
+        //     ["name" => "Tania", "email" => "tania@gmail.com", "phone" => "777888", "address" => "Mymensingh", "photo" => "tania.jpg"],
+        //     ["name" => "Nusrat", "email" => "nusrat@gmail.com", "phone" => "888999", "address" => "Dhaka", "photo" => "nusrat.jpg"],
+        //     ["name" => "Arif", "email" => "arif@gmail.com", "phone" => "999000", "address" => "Sylhet", "photo" => "arif.jpg"],
+        //     ["name" => "Kamal", "email" => "kamal@gmail.com", "phone" => "112233", "address" => "Khulna", "photo" => "kamal.jpg"],
+        //     ["name" => "Rumi", "email" => "rumi@gmail.com", "phone" => "223344", "address" => "Dhaka", "photo" => "rumi.jpg"],
+        //     ["name" => "Mehedi", "email" => "mehedi@gmail.com", "phone" => "334455", "address" => "Rajshahi", "photo" => "mehedi.jpg"],
+        //     ["name" => "Shanto", "email" => "shanto@gmail.com", "phone" => "445566", "address" => "Chittagong", "photo" => "shanto.jpg"],
+        //     ["name" => "Alif", "email" => "alif@gmail.com", "phone" => "556677", "address" => "Barishal", "photo" => "alif.jpg"],
+        //     ["name" => "Borna", "email" => "borna@gmail.com", "phone" => "667788", "address" => "Mymensingh", "photo" => "borna.jpg"],
+        //     ["name" => "Omar", "email" => "omar@gmail.com", "phone" => "778899", "address" => "Dhaka", "photo" => "omar.jpg"],
+        //     ["name" => "Selim", "email" => "selim@gmail.com", "phone" => "889900", "address" => "Rangpur", "photo" => "selim.jpg"],
+        //     ["name" => "Akash", "email" => "akash@gmail.com", "phone" => "990011", "address" => "Sylhet", "photo" => "akash.jpg"],
+        //     ["name" => "Lamia", "email" => "lamia@gmail.com", "phone" => "100200", "address" => "Chittagong", "photo" => "lamia.jpg"]
+        // ]);
+
+        
+    }
+}
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,6 +1,6 @@
 
- <h1>Student Id: {{$id}}</h1>
- <h1>Student name: {{$name}}</h1>
+ <h1>Student Id=> [[$id]]</h1>
+ <h1>Student name=> [[$name]]</h1>
 <table>
     <tr>
         <th>Id</th>
@@ -9,8 +9,11 @@
      @foreach($student as $key=>$student)
 
     <tr>
-        <th>{{$key}}</th>
-        <th>{{$student}}</th>
+        <th>[[$key]]</th>
+        <th>[[$student]]</th>
      </tr>
      @endforeach
 </table>
+
+
+
