7c6ca5d065de8703b9a0cea0e27670e2d4589c1f
Wdpf-66 <ccsl61588@gmail.com>
2025-12-17 11:44:27 +0600

    CRUD operation , Pagination , eloquent basic

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,14 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
-   
-   function index(){
-       return "I am form Customer controller";
-   }
+
+    function index()
+    {
+        // $customers = Customer::all()->toArray();
+        // echo '<pre>';
+        // print_r( $customers);
+        // echo '</pre>';
+        // return view("customer.index", ["customers"=>$customers]);
+
+        //  query builder
+        // $customers= DB::table("customers")->get();
+        // $customers= DB::select("select * from lav_customers");
+
+        $customers = Customer::orderBy("id","desc")->paginate(8);
+        return view("customer.index", compact("customers"));
+    }
 
 
+
+    function create(){
+        return view("customer.create");
+    }
+    function edit($id){
+        $customer = Customer::find($id);
+        return view("customer.edit", compact("customer"));
+    }
+
+
+    function save(Request $request){
+      print_r($request->all());
+
+      $customer= new Customer();
+      $customer->name= $request->name;
+      $customer->email= $request->email;
+      $customer->phone= $request->phone;
+      $customer->address= $request->address;
+    //   $customer->photo= $request->photo;
+      $customer->save();
+
+    //   echo "saved";
+     return redirect("customer");
+    }
+
+
+    function update(Request $request, $id){
+      print_r($request->all());
+
+      $customer= Customer::find($id);
+      $customer->name= $request->name;
+      $customer->email= $request->email;
+      $customer->phone= $request->phone;
+      $customer->address= $request->address;
+     //   $customer->photo= $request->photo;
+      $customer->update();
+
+      //   echo "saved";
+     return redirect("customer");
+    }
+
+
+
+    function delete($id){
+      //  Customer::find($id)->delete();
+       $customer = Customer::find($id);
+       $customer->delete();
+       return redirect("customer");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get("/harun", function(){
 //    // return "Student Id is {$id} and His name is {$name} ";
 //    $student =["Hasan", "Masud", "M A Jalil", "Mashrur"];
 //    return view("students", ["id"=> $id,"name"=>$name ,"student"=>$student]);
-   
+
 // });
 
 Route::get("/customers", [CustomerController::class, "index"]);
@@ -33,6 +33,21 @@ Route::get("/student/update/{id}/{name}", [StudentController::class, 'update']);
 Route::get("/student/delete/{id}", [StudentController::class, 'delete']);
 
 
+Route::prefix("customer")->controller(CustomerController::class)->group(function(){
+    Route::get("/", "index");
+    Route::get("create", "create");
+    Route::post("save", "save");
+    Route::delete("delete/{id}", "delete");
+    Route::get("edit/{id}", "edit");
+    Route::post("update/{id}", "update");
+});
+
+// Route::get("/customer", [CustomerController::class, "index"]);
+// Route::get("/customer/create", [CustomerController::class, "create"]);
+// Route::post("/customer/save", [CustomerController::class, "save"]);
+// Route::delete("/customer/delete/{id}", [CustomerController::class, "delete"]);
+// Route::get("/customer/edit/{id}", [CustomerController::class, "edit"]);
+// Route::post("/customer/update/{id}", [CustomerController::class, "update"]);
 
 Route::fallback(function(){
    return "404 No Route matched";
